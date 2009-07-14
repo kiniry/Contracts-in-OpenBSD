bb8e22761262aca2e134caf6080a3f8053b38979
muratto <muratto@aa7bbac1-9153-487f-ab27-dea8792d0b3c>
2009-07-14 08:55:57 +0000

    this was incomplete, verified range for index works and cleaned up.

--- test/sal/7.c
+++ test/sal/7.c
@@ -1,15 +1,16 @@
-// __out t, size_t size can assign any index ????
+// __out t, size_t size can assign any index
 
 //#pragma JessieIntegerModl(modulo)
 
 /*@ requires
   @   size > 0 && size < INT_MAX && \valid(t) && \valid_range(t,0,size-1);
   @ behavior zero:
-  @   assumes (t == NULL) || (size <= 0);
+  @   assumes (t == NULL);
   @   assigns \nothing;
   @ behavior normal:
-  @   assumes size > 0;
+  @   assumes \valid(t);
   @   assigns t[0..size-1];
+  @ disjoint behaviors zero, normal; // not strictly necessary but for completeness. BTW this is a comment for annotation.
 */
 void f5(char *t, int size) {
   if ( t && size > 0)
