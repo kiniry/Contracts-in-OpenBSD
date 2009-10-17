60ef35173ba8f25f50e10fa80dd69d1c61644de5
muratto <muratto@aa7bbac1-9153-487f-ab27-dea8792d0b3c>
2009-10-17 12:02:12 +0000

    writing specs for chosen funcs.

---
+++ obsdc/specs/2-With_man/string.h
@@ -0,0 +1,52 @@
+// specs for top functions without looking at man or code - cleaned out rest of the functions out.
+// had to add missing parameter names.
+// had to create the include file structure and copy include files.
+
+#ifndef _STRING_H_
+#define	_STRING_H_
+
+#include <sys/cdefs.h>
+#include <machine/_types.h>
+
+#ifndef	_SIZE_T_DEFINED_
+#define	_SIZE_T_DEFINED_
+typedef	__size_t	size_t;
+#endif
+
+#ifndef	NULL
+#ifdef 	__GNUG__
+#define	NULL	__null
+#else
+#define	NULL	0L
+#endif
+#endif
+
+__BEGIN_DECLS
+int	 memcmp(const void *, const void *, size_t);
+char	*strcat(char *, const char *);
+char	*strchr(const char *, int);
+int	 strcmp(const char *, const char *);
+char	*strcpy(char *, const char *);
+
+/*@ requires valid_string(s);
+  @ assigns \nothing;
+  @ ensures \valid_range(s, 0, \result) && s[\result] == '\0' && \forall unsigned int k; 0 <= k < \result && s[k] != '\0';
+  @*/
+size_t	 strlen(const char *s);
+char	*strncat(char *, const char *, size_t)
+		__attribute__ ((__bounded__(__string__,1,3)));
+int	 strncmp(const char *, const char *, size_t);
+char	*strncpy(char *, const char *, size_t)
+		__attribute__ ((__bounded__(__string__,1,3)));
+char	*strrchr(const char *, int);
+char	*strstr(const char *, const char *);
+
+
+#if __BSD_VISIBLE || __XPG_VISIBLE >= 420
+int	 strcasecmp(const char *, const char *);
+int	 strncasecmp(const char *, const char *, size_t);
+char	*strdup(const char *);
+#endif
+
+
+#endif /* _STRING_H_ */
